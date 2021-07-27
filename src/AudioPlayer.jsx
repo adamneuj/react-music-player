3f26a7cbf98332a2f16cb7972d43811874831da6
Adam Neujahr <adamneujahr@gmail.com>
2021-07-27 16:46:10 -0500

    Add audio controls and backdrop changes for changing tracks.

--- src/AudioPlayer.jsx
+++ src/AudioPlayer.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
+import AudioControls from './AudioControls';
+import Backdrop from './Backdrop';
 import "./styles.css";
 
 function AudioPlayer({tracks}) {
@@ -19,12 +21,42 @@ function AudioPlayer({tracks}) {
 	const { duration } = audioRef.current;
 
     const toPreviousTrack = () => {
-        console.log('TODO go to previous');
-      }
+        if (index - 1 < 0) {
+            setIndex(tracks.length - 1);
+        } else {
+            setIndex(index - 1);
+        }
+      };
     
     const toNextTrack = () => {
-        console.log('TODO go to next');
-      }
+        if (index < tracks.length - 1) {
+            setIndex(index + 1);
+        } else {
+            setIndex(0);
+        }
+      };
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioRef.current.play();
+        } else {
+            audioRef.current.pause();
+        }
+    }, [isPlaying]);
+
+    //Changes the track when going forward or previous.
+    useEffect(() => {
+        audioRef.current.pause();
+
+        audioRef.current = new Audio(audioSource);
+
+        if(isReady.current) {
+            audioRef.current.play();
+            setIsPlaying(true);
+        } else {
+            isReady.current = true;
+        }
+    }, [index]);
 
     return (
         <div className="audio-player">
@@ -36,7 +68,18 @@ function AudioPlayer({tracks}) {
 			  />
             <h2 className="title">{title}</h2>
             <h3 className="artist">{artist}</h3>
+            <AudioControls
+                isPlaying={isPlaying}
+                onPrevClick={toPreviousTrack}
+                onNextClick={toNextTrack}
+                onPlayPauseClick={setIsPlaying}
+            />
             </div>
+            <Backdrop
+                trackIndex={index}
+                activeColor={color}
+                isPlaying={isPlaying}
+            />
         </div>
     );
 }
--- src/tracks.js
+++ src/tracks.js
@@ -1,20 +1,22 @@
 import sthArtwork from "./assets/sthartwork.jpg"
+import positivTrumpetArt from "./assets/positiv-trumpet.jpg"
 import sthMusic from "./assets/Save the Hero - Left You Behind.mp3"
+import positivTrumpet from "./assets/John_Sib_positiv-trumpet.mp3"
 
 const tracks = [
+    {
+        title: "positiv trumpet",
+        artist: "John Sib",
+        audioSource: positivTrumpet,
+        imageSource: positivTrumpetArt,
+        color: "#00aabf"
+    },
     {
         title: "Left You Behind",
         artist: "Save the Hero",
         audioSource: sthMusic,
         imageSource: sthArtwork,
-        color: "#c92338"
-    },
-    {
-        title: "Dexter",
-        artist: "Awake/Alive",
-        audioSource: "",
-        imageSource: "",
-        color: ""
+        color: "#df2d3b"
     },
     {
         title: "",
