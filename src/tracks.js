c14d186f1a9e5b3a19eb7439d4908097205e897b
Adam Neujahr <adamneujahr@gmail.com>
2021-07-27 20:28:19 -0500

    Add another track, fix linting

--- src/tracks.js
+++ src/tracks.js
@@ -1,8 +1,10 @@
 import sthArtwork from "./assets/sthartwork.jpg"
 import positivTrumpetArt from "./assets/positiv-trumpet.jpg"
+import futurebassArt from "./assets/futurebass.jpg"
 
 import sthMusic from "./assets/Save the Hero - Left You Behind.mp3"
 import positivTrumpet from "./assets/John_Sib_positiv-trumpet.mp3"
+import futureBass from "./assets/jorikbasov_the-future-bass.mp3"
 
 const tracks = [
     {
@@ -13,18 +15,18 @@ const tracks = [
         color: "#00aabf"
     },
     {
+        title: "the future bass",
+        artist: "jorikbasov",
+        audioSource: futureBass,
+        imageSource: futurebassArt,
+        color: "#a20079"
+    },
+    {
         title: "Left You Behind",
         artist: "Save the Hero",
         audioSource: sthMusic,
         imageSource: sthArtwork,
         color: "#df2d3b"
-    },
-    {
-        title: "",
-        artist: "",
-        audioSource: "",
-        imageSource: "",
-        color: ""
     }
 ];
 
